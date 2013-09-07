f1f185f82b0658b0763d5adaba1869c1d98932f5
Jacek Lehmann <vonschlager@wp.pl>
2013-09-07 21:49:38 +0200

    clean up

--- src/Application.hs
+++ src/Application.hs
@@ -16,21 +16,14 @@ import Snap.Snaplet.SqliteSimple
 
 ------------------------------------------------------------------------------
 data App = App
-    { _heist :: Snaplet (Heist App)
-    , _sess :: Snaplet SessionManager
-    , _auth :: Snaplet (AuthManager App)
-    , _db :: Snaplet Sqlite
+    { _db :: Snaplet Sqlite
     }
 
 makeLenses ''App
-
-instance HasHeist App where
-    heistLens = subSnaplet heist
 
 instance HasSqlite (Handler b App) where
     getSqliteState = with db get
 
 ------------------------------------------------------------------------------
 type AppHandler = Handler App App
-
 
